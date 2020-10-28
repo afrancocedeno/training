c454b37a4ad3c8c517fe0da2ada1351c0c3a7917
afrancocedeno <2208@holbertonschool.com>
2020-10-28 15:52:12 +0000

    01 code_fix betty

--- structures/studentdata.c
+++ structures/studentdata.c
@@ -1,31 +1,21 @@
 #include <stdio.h>
-
-/**
- * struct infostudent - user defined datatype
- *
- */
-
-struct infostudent
-{
-	char *firstname;
-	char *lastname;
-	int course;
-	float age;
-};
+#include "training.h"
 
 int main(void)
 {
-
 /*declaration - student1 variable with user datatype infostudent*/
 	struct infostudent student1;
 
 /*accesing to user defined datatype members (elements)*/
-	student1.firstname = "Alejo";
-	student1.lastname = "Cedeño";
-	student1.course = 13;
-	student1.age = 25.5;
+student1.firstname = "Alejo";
+student1.lastname = "Cedeño";
+student1.course = 13;
+student1.age = 25.5;
 
-	printf("%s", student1.firstname);
+printf("%s", student1.firstname);
+printf("%s", student1.firstname);
+printf("%s", student1.firstname);
+printf("%s", student1.firstname);
 
 	return (0);
 }

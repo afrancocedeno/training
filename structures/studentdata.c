352aece6001f36242c668208705fb08026f1e049
afrancocedeno <2208@holbertonschool.com>
2020-10-29 00:10:39 +0000

    04 code_fix output

--- structures/studentdata.c
+++ structures/studentdata.c
@@ -1,29 +1,26 @@
 #include <stdio.h>
 #include "training.h"
 
+/**
+ * main - entry point function.
+ * Return: (0) for succes function.
+ */
+
 int main(void)
 {
-
-	struct infostudent /*local structure*/
-	{
-		char *firstname;
-		char *lastname;
-		int course;
-		float age;
-	};
 /*declaration - student1 variable with user datatype infostudent*/
 	struct infostudent student1;
-
+	
 /*accesing to user defined datatype members (elements)*/
-student1.firstname = "Alejo";
-student1.lastname = "Cedeño";
-student1.course = 13;
-student1.age = 25.5;
-
-printf("%s", student1.firstname);
-printf("%s", student1.firstname);
-printf("%s", student1.firstname);
-printf("%s", student1.firstname);
-
+	student1.firstname = "Alejo";
+	student1.lastname = "Cedeño";
+	student1.course = 13;
+	student1.age = 25.5;
+	
+	printf("%s\n", student1.firstname);
+	printf("%s\n", student1.lastname);
+	printf("%i\n", student1.course);
+	printf("%.2f\n", student1.age);
+	
 	return (0);
 }

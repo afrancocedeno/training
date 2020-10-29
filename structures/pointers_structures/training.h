825bd5a43592d855a7284c587dc1292c8339b4b5
afrancocedeno <2208@holbertonschool.com>
2020-10-29 00:58:33 +0000

    00 file_create

---
+++ structures/pointers_structures/training.h
@@ -0,0 +1,18 @@
+#ifndef __TRAINING_H__
+#define __TRAINING_H__
+
+/**
+ * struct infostudent - user defined datatype.
+ * @firstname: pointer char with firstname.
+ * @lastname: pointer char with lastname.
+ * @course: int variable with course.
+ * @age: intput variable with age.
+ */
+struct infostudent
+{
+	char *firstname;
+	char *lastname;
+	int course;
+	float age;
+};
+#endif

42483b9c256d8932da7de54e5976f446533fed73
afrancocedeno <2208@holbertonschool.com>
2020-10-29 02:58:39 +0000

    00 file_create

---
+++ structures/typedef/studentdata.c
@@ -0,0 +1,28 @@
+#include "training.h"
+#include <stdio.h>
+
+/**
+ * main - entry point function.
+ * Return: 0 succes.
+ */
+
+int main(void)
+{
+/*not necesary to declare struct again*/
+	infostudent student1;
+	infostudent *ptr_student1;
+
+	*ptr_student1 = &student1;
+
+	(*ptr_student1).firstname = "Alejo";
+	(*ptr_student1).lastname = "Cedeño";
+	ptr_student1->course = 13;
+	ptr_student1->age = 25.8;
+	printf("%s\n", (*ptr_student1).firstname);
+	printf("%s\n", (*ptr_student1).lastname);
+	printf("%i\n", student1.course);
+	printf("%.2f\n", student1.age);
+
+	return (0);
+
+}

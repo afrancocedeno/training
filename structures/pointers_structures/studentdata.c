03649bc1f9da72b660f63c74fc2660d19e46e7ee
afrancocedeno <2208@holbertonschool.com>
2020-10-29 02:36:57 +0000

    02 code_fix header

--- structures/pointers_structures/studentdata.c
+++ structures/pointers_structures/studentdata.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include "training.h"
 
 /**
  * main -entry point function.
@@ -6,16 +7,20 @@
  */
 int main(void)
 {
+/*declaration - student1 variable with user defined datatype*/
 	struct infostudent student1;
+
+/*declaration - pointer with infostructure datatype*/
 	struct infostudent *ptr_student1;
 
+/*asign - pointer to loval variable both with the same datatype*/
 	ptr_student1 = &student1;
 
 	(*ptr_student1).firstname = "Alejo";
 	(*ptr_student1).lastname = "Cedeño";
 	ptr_student1->course = 13;
 	ptr_student1->age = 25.8;
-
+/*pointer must be casted inside parentesis*/
 	printf("%s\n", (*ptr_student1).firstname);
 	printf("%s\n", (*ptr_student1).lastname);
 	printf("%i\n", student1.course);

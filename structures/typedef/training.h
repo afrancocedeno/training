3691e4acc55a35657008008bf57ef4c54d5ee1f7
afrancocedeno <2208@holbertonschool.com>
2020-10-29 02:58:54 +0000

    00 file_create

---
+++ structures/typedef/training.h
@@ -0,0 +1,22 @@
+#ifndef __TRAINING_H__
+#define __TRAINING_H__
+
+/**
+ * struct infostudent - user defined datatype.
+ * @firstname: pointer char with firstname.
+ * @lastname: pointer char with lastname.
+ * @course: int variable with course.
+ * @age: intput variable with age.
+ */
+
+/*global structure, outside the main but not inside .c file*/
+/*alias typedef for (struct infostudent)*/
+typedef struct infostudent
+{
+	char *firstname;
+	char *lastname;
+	int course;
+	float age;
+} infostudent;
+
+#endif

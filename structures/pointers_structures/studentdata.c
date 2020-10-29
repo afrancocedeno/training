2fa76c4fad15ca397edf2e98eec10732877f91bb
afrancocedeno <2208@holbertonschool.com>
2020-10-29 00:43:22 +0000

    00 file_create

---
+++ structures/pointers_structures/studentdata.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+struct infostudent
+{
+	char *firstname;
+	char *lastname;
+	int course;
+	float age;
+};
+
+int main(void)
+{
+	struct infostudent student1;
+	struct infostudent *ptr_student1;
+
+	ptr_student1 = &student1;
+	
+	(*ptr_student1).firstname = "Alejo";
+	(*ptr_student1).lastname= "Cedeño";
+	ptr_student1->course = 13;
+	ptr_student1->age = 25.8;
+
+	printf("%s\n", (*ptr_student1).firstname);
+	printf("%s\n", (*ptr_student1).lastname);
+	printf("%i\n", student1.course);
+	printf("%.2f\n", student1.age);
+
+	return (0);
+}

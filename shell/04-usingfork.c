603b733c9b7ad81ff0f9d4320d33d2b2e604fc51
afrancocedeno <afrancocedeno@gmail.com>
2020-11-20 12:18:26 -0500

    01 | code try | output ok

--- shell/04-usingfork.c
+++ shell/04-usingfork.c
@@ -1,9 +1,21 @@
+/* libs for process functions -  fork */
 #include <sys/types.h>
 #include <unistd.h>
+/* lib for std funcitons - printf */
+#include <stdio.h>
 
+/**
+ * main - enry point function that is a process and his child.
+ *
+ * Return: 0 for success.
+ */
 int main(void)
 {
 	int id = fork();
 
-	
+	if (id == 0)
+		printf("the id of the child process is: %d\n", id);
+	else
+		printf("the id of the main (parent) process is: %d\n", id);
+	return (0);
 }

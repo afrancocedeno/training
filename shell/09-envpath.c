8db2ae9bbb492e57d6502b4be7306de687172434
afrancocedeno <afrancocedeno@gmail.com>
2020-11-24 16:57:20 -0500

    01 | file update | seudo code

--- shell/09-envpath.c
+++ shell/09-envpath.c
@@ -10,6 +10,10 @@
  */
 int main(void)
 {
-
+/* identificar PATH en doble puntero **env */
+/* tokenizar con "=" PATH = __rutas__ */
+/* identificar segundo token "__rutas__" */
+/* tokenizar string resultante con ":"" separando rutas */
+/* identificar la ruta solicitada */
 return (0);
 }

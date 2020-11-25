5aff58f86803ecba2c3daedeb343c9793e4c4d61
afrancocedeno <afrancocedeno@gmail.com>
2020-11-24 20:59:43 -0500

    06 | code try | is in var 24, PATH separated from line with our _strtok

--- shell/09-envpath.c
+++ shell/09-envpath.c
@@ -2,6 +2,9 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <string.h> /* strok headers */
+#include <stdlib.h>
+
+char **tokens(char *str, const char *delimitador);
 
 /**
  * main - entry point funciton.
@@ -10,12 +13,41 @@
  */
 int main(void)
 {
-	/* for (; environ[i] != NULL; i++) printf("[%i] : %s\n", i, environ[i]); */
-	printf("%i\n", **environ);
-/* identificar PATH en doble puntero **env */
-/* tokenizar con "=" PATH = __rutas__ */
-/* identificar segundo token "__rutas__" */
-/* tokenizar string resultante con ":"" separando rutas */
-/* identificar la ruta solicitada */
-return (0);
+	printf("%s\n", environ[24]);
+	printf("%s\n", *tokens(environ[24], "="));
+	/* identificar PATH en doble puntero **env */
+	/* tokenizar con "=" PATH = __rutas__ */
+	/* identificar segundo token "__rutas__" */
+	/* tokenizar string resultante con ":"" separando rutas */
+	/* identificar la ruta solicitada */
+	return (0);
+}
+
+/**
+ * tokens - function that tokenize the user instructions.
+ * @str: global variable input with user line.
+ * @delimitador: global variable input with string separator.
+ *
+ * Return: char** with array of tokens.
+ */
+char **tokens(char *str, const char *delimitador)
+{
+	char **array = NULL;
+	int i, token = 0;
+
+	/*contador de tokens*/
+	for (i = 0; str[i] != '\0'; i++)
+		if ((str[i] == 32 && str[i + 1] != 32) || str[i + 1] == '\0')
+			token++;
+	/*asignacion de memoria*/
+	array = malloc((token + 1) * sizeof(char *));
+	if (array == NULL)
+		exit(-1);
+	i = 0;
+	/*primer llamado strtok*/
+	array[i] = strtok(str, delimitador);
+	for (i = 1; i < token ; i++)
+		array[i] = strtok(NULL, delimitador);
+	array[token] = NULL;
+	return (array);
 }

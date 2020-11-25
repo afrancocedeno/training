ac749aba412f4edef855aa6ecfc2509850cb1655
afrancocedeno <afrancocedeno@gmail.com>
2020-11-25 12:36:20 -0500

    14 | code try | closer compare in one loop, still var 25 is a constant

--- shell/09-envpath.c
+++ shell/09-envpath.c
@@ -12,21 +12,13 @@ int main(void)
 {
 	char *env_var = "PATH";
 	int i = 0;
-
-	printf("from PATH: \n");
+	/* tokenizar con "=" PATH = __rutas__ >>> *(strtok(*(environ + j), "=") + i)*/
 	for (; *(strtok(*(environ + 25), "=") + i); i++)
-		printf("[%d] : %c\n", i, *(strtok(*(environ + 25), "=") + i));
-	printf("from main: \n");
-	for (i = 0; *(env_var + i) != '\0'; i++)
-		printf("[%d] : %c\n", i, *(env_var + i));
-
-	/* printf("%s\n", strtok(environ[25], "="));
-	printf("%c\n", *(strtok(*(environ + 25), "=") + 1)); */
-
+		printf("PATH : %c; main : %c\n", *(strtok(*(environ + 25), "=") + i), *(env_var + i));
 	/* identificar PATH en doble puntero **env */
-	/* tokenizar con "=" PATH = __rutas__ */
-	/* identificar segundo token "__rutas__" */
+
 	/* tokenizar string resultante con ":"" separando rutas */
+	/* identificar segundo token "__rutas__" */
 	/* identificar la ruta solicitada */
 	return (0);
 }

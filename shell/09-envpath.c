9463e0ca9317827e4bb4060d738d45625dc6acfa
afrancocedeno <2208@holbertonschool.com>
2020-11-26 04:42:04 +0000

    20 | code try | almost get the PATH, does not work for vagrant

--- shell/09-envpath.c
+++ shell/09-envpath.c
@@ -11,26 +11,29 @@
 int main(void)
 {
 	char *env_var = "PATH";
-	char **var_keeper = NULL;
 	int i = 0, j = 0, input_token_length = 0, env_token_length = 0;
 
 	input_token_length = (int)strlen(env_var);
 	env_token_length = (int)strlen(strtok(*(environ + 25), "="));
-
-	for (; (*(environ + j) + i) != NULL; j++)
+	for (; (strtok(*(environ + j), "=") + i) != NULL; j++)
 	{
 		printf("[%d] : ", j);
-		for (; *(env_var + i) != '\0'; i++)
-			/* if ((*(strtok(*(environ + j), "=") + i) == *(env_var + i))) */
-			if (environ[j][i] == 'P' && environ[j][i + 1] == 'A' && environ[j][i + 2] == 'T' && environ[j][i + 3] == 'H')
-				**(var_keeper + i) = environ[j][i];
-				printf("%c", *(*(environ + j)) + i);
+		for (; *(strtok(*(environ + j), "=") + i) != '\0'; i++)
+			if ((*(strtok(*(environ + j), "=") + i) == *(env_var + i)) && (input_token_length == env_token_length))
+			{
+				printf("%c", *(strtok(*(environ + j), "=") + i));
+			}
 		printf("\n");
 		i = 0;
 	}
-	printf("%s\n", *var_keeper);
 	return (0);
 }
+/* 1. identificar PATH en doble puntero **env */
+/* 2. tokenizar con "=" PATH = __rutas__ > *(strtok(*(environ + j), "=") + i)*/
+/* 3. tokenizar string resultante con ":"" separando rutas */
+/* 4. identificar segundo token "__rutas__" */
+/* 5. identificar la ruta solicitada */
+/* 6. buffer ?? condition with '\0' ?? */
 /* 1. identificar PATH en doble puntero **env */
 /* 2. tokenizar con "=" PATH = __rutas__ > *(strtok(*(environ + j), "=") + i)*/
 /* 3. tokenizar string resultante con ":"" separando rutas */

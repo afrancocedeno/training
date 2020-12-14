909e44640a9f719d7536179b3390b1c8292594cd
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-14 17:27:43 +0000

    03 | file update | :.2f for two decimals on format

--- python/unit_converter.py
+++ python/unit_converter.py
@@ -2,4 +2,4 @@
 number_get = input('type the mile to convert: ')
 number_get = int(number_get)
 number_get = 1.609934 * number_get
-print("result: {}km".format(number_get))
+print("result: {:.2f}km".format(number_get))

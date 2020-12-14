b0126a561cbae4b986a15e48c50e0ae04bb80d12
Alejandro Franco Cedeño <afrancocedeno@gmail.com>
2020-12-14 16:22:23 +0000

    00 | file create | .split way error that prints input every time

---
+++ python/two_inputs.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+num1, num2 = input('Type two numbers to operate: ').split()
+
+num1 = int(num1)
+num2 = int(num2)
+
+addition = num1 + num2
+difference = num1 - num2
+product = num1 * num2
+quotient = num1 / num2
+reminder = num1 % num2
+
+print('addition: {} + {} = {}'.format(num1, num2, addition))
+print('difference: {} - {} = {}'.format(num1, num2, difference))
+print('product: {} * {} = {}'.format(num1, num2, product))
+print('quotient: {} / {} = {}'.format(num1, num2, quotient))
+print('reminder: {} % {} = {}'.format(num1, num2, reminder))
+
